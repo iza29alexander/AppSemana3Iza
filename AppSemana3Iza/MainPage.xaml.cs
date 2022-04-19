07a0e810db20b60622119bee9bcaff4d6c907575
Edi <alex-edy175@hotmail.com>
2022-04-18 21:14:45 -0500

    Agregar archivos de proyecto.

---
+++ AppSemana3Iza/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppSemana3Iza
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btn2_Clicked(object sender, EventArgs e)
+        {
+            string user = txtuser.Text;
+            string pass = txtpass.Text;
+
+            await Navigation.PushAsync(new Page2(user, pass));
+        }
+    }
+}
